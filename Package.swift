f703b96699cd4cdad9855e8092159ad7444b6ef4
Ilia Snezhkov <iasnezhkov@gmail.com>
2026-09-08 05:45:52 +0000

    Release v0.2.0-rc1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "sudachi_swiftFFI",
-            url: "https://github.com/iasnezhkov/sudachi-swift/releases/download/v0.1.1/Sudachi.xcframework.zip",
-            checksum: "5f17105e7ec602413fc8c72e6a44d273758b36fa8da17edc3f6b0cc43983984e"
+            url: "https://github.com/iasnezhkov/sudachi-swift/releases/download/v0.2.0-rc1/Sudachi.xcframework.zip",
+            checksum: "87789f450beecb7854b0cfecb9247e40ee505a24d538a1dcad368a0203322ebf"
         ),
         .target(
             name: "Sudachi",
